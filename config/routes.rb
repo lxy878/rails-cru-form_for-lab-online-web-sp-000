29406d67d70e8b5272622bdfe2075a5ad401474b
Xiaoyi Liu <lxy878@gmail.com>
2020-06-19 05:37:47 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :genres, only: [:show]
+  resources :genres, only: [:show, :new, :create]
   resources :artists
   resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
