2fd6d8e37826e46677e227bf6d23cfccb6238ecd
Xiaoyi Liu <lxy878@gmail.com>
2020-06-19 07:56:11 +0000

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,28 @@
+class GenresController < ApplicationController
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    genre = Genre.create(post_params)
+    redirect_to genre_path(genre)
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    genre = Genre.find(params[:id])
+    genre.update(post_params)
+    redirect_to genre_path(genre)
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def post_params
+    params.require(:genre).permit(:name)
+  end
+end
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,38 @@
+class SongsController < ApplicationController
+  def new
+    @song = Song.new
+    @artists = Artist.all
+    @genres = Genre.all
+  end
+  
+  def create
+    song = Song.create(post_params)
+    redirect_to song_path(song)
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+    @artists = Artist.all
+    @genres = Genre.all
+  end
+
+  def update
+    song = Song.find(params[:id])
+    song.update(post_params)
+    redirect_to song_path(song)
+  end
+
+  def show
+    @song = Song.find(params[:id])
+    @artist = @song.artist
+    @genre = @song.genre
+  end
+
+  def index
+    @songs = Song.all
+  end
+
+  def post_params
+    params.require(:song).permit(:name, :artist_id, :genre_id)
+  end
+end
