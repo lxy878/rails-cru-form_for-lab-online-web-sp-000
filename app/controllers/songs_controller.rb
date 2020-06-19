58eecb4b84b58c55e42dfe16a3fa05c7646013ff
Xiaoyi Liu <lxy878@gmail.com>
2020-06-19 05:43:30 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,21 @@
 class SongsController < ApplicationController
+  def new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def index
+  end
 end
