aa4c4590c11fd366ad153540ccf67e4d27a34b05
Xiaoyi Liu <lxy878@gmail.com>
2020-06-19 05:12:25 +0000

    Automatically backed up by Learn

--- db/migrate/20200619051052_create_artists.rb
+++ db/migrate/20200619051052_create_artists.rb
@@ -1,8 +1,8 @@
 class CreateArtists < ActiveRecord::Migration[5.0]
   def change
     create_table :artists do |t|
-
-      t.timestamps
+      t.string :name
+      t.text :bio
     end
   end
 end
